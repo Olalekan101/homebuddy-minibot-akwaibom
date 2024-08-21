3872d939c1f328a229b9aa7cbd8892d3cabc7673
Olalekan <123008739+Olalekan101@users.noreply.github.com>
2024-08-21 19:22:56 +0100

    v3

--- api/telegram/route.js
+++ api/telegram/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import TelegramBot from 'node-telegram-bot-api';
 
-const token = process.env.TELEGRAM_BOT_TOKEN;
+const token = process.env.NEXT_PUBLIC_TELEGRAM_BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: false });
 
 export async function POST(req) {
